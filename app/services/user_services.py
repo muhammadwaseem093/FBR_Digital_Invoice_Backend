28c5f9c0fd0831a0ac67cc062cf200d19b9b479e
Muhammad Waseem <muhammadwaseem093@gmail.com>
2025-07-19 13:57:17 +0500

    add get_user_by_email in services

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -23,3 +23,5 @@ def create_user(
     db.commit()
     db.refresh(db_user)
     return db_user
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
