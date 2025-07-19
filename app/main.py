df9f1e56dc6d05bbfec3d148dcf8327f431dbad2
Muhammad Waseem <muhammadwaseem093@gmail.com>
2025-07-19 14:21:46 +0500

    update main.py for cors

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.paktaxtools.com/"],  # frontend origin
+    allow_origins=["https://www.paktaxtools.com"],  # frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
